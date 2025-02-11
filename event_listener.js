aa69a583640600bc2381de6283f739c29ac4a79a
darkpotter1 <harryniki@gmail.com>
2025-02-11 16:42:59 +0200

    upgrade2.0

--- event_listener.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('searchInput').addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-      document.getElementById('btn').click();
-    }
-});
---
+++ scripts.js
@@ -0,0 +1,34 @@
+document.getElementById('searchInput').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      document.getElementById('btn').click();
+    }
+});
+
+        function searchXML() {
+            var input = document.getElementById("searchInput").value;
+            var resultDiv = document.getElementById("result");
+            resultDiv.innerHTML = ""; // Clear previous results
+
+            // New XML http request
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "data.xml", true);  
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var xmlDoc = xhr.responseXML;
+                    var players = xmlDoc.getElementsByTagName("PLAYER");
+                    var found = false;
+                    for (var i = 0; i < players.length; i++) {
+                        var name = players[i].getAttribute("name");
+                        if (name.toLowerCase()===(input.toLowerCase())) {
+                            found = true;
+                            resultDiv.innerHTML += "<p>Намерено съвпадение: " + name + " (настоящо: " + players[i].getAttribute("net") + ", общо: " + players[i].getAttribute("total") + ")</p>";
+                        }
+                    }
+                    if (!found) {
+                        resultDiv.innerHTML = "<p>Няма намерени съвпадения.</p>";
+                    }
+                }
+            };
+            xhr.send();
+        }
+    
