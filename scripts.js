d7860644d0a115983104da90717b0d720cb2d48b
darkpotter1 <harryniki@gmail.com>
2025-02-14 16:54:22 +0200

    added date/time

--- scripts.js
+++ scripts.js
@@ -15,13 +15,14 @@ document.getElementById('searchInput').addEventListener('keydown', function(even
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4 && xhr.status === 200) {
                     var xmlDoc = xhr.responseXML;
+                    const date = xmlDoc.documentElement.getAttribute('time');
                     var players = xmlDoc.getElementsByTagName("PLAYER");
                     var found = false;
                     for (var i = 0; i < players.length; i++) {
                         var name = players[i].getAttribute("name");
                         if (name.toLowerCase()===(input.toLowerCase())) {
                             found = true;
-                            resultDiv.innerHTML += "<span>Намерено съвпадение: " + name + "</br> (настоящо: " + players[i].getAttribute("net") + ", общо: " + players[i].getAttribute("total") + ")</span>";
+                            resultDiv.innerHTML += "<span>Намерено съвпадение: " + name + "</br><b>(настоящо: " + players[i].getAttribute("net") + ", общо: " + players[i].getAttribute("total") + ")</b></br></br><i>обновено на "+ date +"</i></span>";
                         }
                     }
                     if (!found) {
